653b43cc870506d031de06c82c9ab67d1804913b
LemonyIce <879935228@qq.com>
2019-07-30 17:11:33 +0800

    function for add a new list

--- src/App.js
+++ src/App.js
@@ -6,15 +6,26 @@ import AppFooter from './js/AppFooter'
 import AppList from'./js/AppList'
 
 class App extends Component {
-    state ={
+    constructor(props){
+      super(props)
+      this.state =  {
         data:this.props.data
+      }
+    }
+    
+    OnAddTodoItem(newItem){
+      var  newdata = this.state.data.concat(newItem);
+      console.log(newdata);
+      this.setState({data:newdata});
     }
   render() {
     const{data} = this.state;
     return (   
       <div className="App">
         <AppHeader />
-        <AppForm />
+        <AppForm 
+          onAddTodoItem={this.OnAddTodoItem.bind(this)}
+        />
         <AppList data={data}/>
         <AppFooter />
       </div>
--- src/js/AppFrom.js
+++ src/js/AppFrom.js
@@ -4,6 +4,18 @@ import '../css/AppButton.css'
 import '../css/AppButton.css'
 
 class AppFrom extends React.Component{
+    handleSubmit(event){
+        event.preventDefault();
+        let text = this.refs.text.value;
+        if(!text.trim()){
+            alert("Input can`t be null")
+            return
+        }else{
+            console.log(text);
+            this.props.onAddTodoItem({"text":text,"complete":false});
+        }
+    }
+    
     render(){
         return(
             <div className="AppFrom">
@@ -17,6 +29,7 @@ class AppFrom extends React.Component{
                         <button 
                             type="submit" 
                             className="ui-button Formbutton"
+                            onClick={this.handleSubmit.bind(this)}
                         >添加</button>
                     </div>
                     
